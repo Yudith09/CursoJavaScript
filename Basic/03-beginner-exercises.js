3e67e5cf1fe55fcaf30c597f96f4769b2732d053
Yudith <yrecio@sifizsoft.com>
2024-07-18 19:16:48 -0400

    Clase 2 de Curso de Java Script

---
+++ Basic/03-beginner-exercises.js
@@ -0,0 +1,64 @@
+/*
+Clase 2 en vídeo | 17/07/2024
+Tipos de datos, operadores y strings
+https://www.twitch.tv/videos/2200149072?t=00h08m02s
+*/
+
+// 1. Escribe un comentario en una línea
+
+// R/ Esto es un comentario de una linea 
+
+// 2. Escribe un comentario en varias líneas
+
+/*R/Esto es un comentario de varias lineas */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let name = "Yudith"
+let firsName = 'Recio'
+let lastName = `Milanes`
+let number = 2
+let decimal = 2.0
+let isTrue = true
+let isFalse = false
+let undefineVaues = undefined
+let nullValues = null
+let symbols = Symbol("number")
+let numberBig= 7675754775455484584684658465646n;
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(name)
+console.log(firsName)
+console.log(lastName)
+console.log(number)
+console.log(decimal)
+console.log(isTrue)
+console.log(isFalse)
+console.log(undefineVaues)
+console.log(nullValues)
+console.log(symbols)
+console.log(numberBig);
+
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof(name))
+console.log(typeof(firsName))
+console.log(typeof(lastName))
+console.log(typeof(number))
+console.log(typeof(decimal))
+console.log(typeof(isTrue))
+console.log(typeof(isFalse))
+console.log(typeof(undefineVaues))
+console.log(typeof(nullValues))
+console.log(typeof(symbols))
+console.log(typeof(numberBig))
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
